10e0276dac1a5ca14173e90d840f93bd24908196
David North <dmhnorth@gmail.com>
2014-07-14 19:11:11 +0100

    Initial commit

---
+++ app/src/main/java/dmhnorth/menuexamples/app/MyDialogFragment.java
@@ -0,0 +1,40 @@
+package dmhnorth.menuexamples.app;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by Dave on 14/07/2014.
+ */
+public class MyDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder theDialog = new AlertDialog.Builder(getActivity());
+
+        theDialog.setTitle("Sample Dialog");
+
+        theDialog.setMessage("Hello I am a Dialog");
+
+        theDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "Clicked OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        theDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "Clicked CANCEL", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return theDialog.create();
+    }
+}
